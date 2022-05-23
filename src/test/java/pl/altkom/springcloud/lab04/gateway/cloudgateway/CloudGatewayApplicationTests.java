7668b547bc03e04ae3349d385a336e9f9be105eb
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-05-23 09:13:37 +0200

    Init

---
+++ src/test/java/pl/altkom/springcloud/lab04/gateway/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.springcloud.lab04.gateway.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
