4394dac6f0435c52c6ecfc4fa9b29ec0b3926367
Artur Mroczkowski <a.mroczkowski@intive.com>
2025-12-01 00:36:30 +0100

    Upgrade spring-cloud, java, spring-boot

--- src/main/java/pl/altkom/springcloud/lab04/gateway/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/pl/altkom/springcloud/lab04/gateway/cloudgateway/CloudGatewayApplication.java
@@ -2,9 +2,7 @@ package pl.altkom.springcloud.lab04.gateway.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class CloudGatewayApplication {
 
